5ebe6c28378a05eea011f968ab8645b1fb259304
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-07-19 07:47:03 +0200

    [scripts] initialize ssh-agent at start of script

--- scripts/yarnSyncIntegriteeKusama.sh
+++ scripts/yarnSyncIntegriteeKusama.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 # simply executes yarn sync with our integritee config and pipe the output to a log file
 # because there is a lot of output when executing the cmd.
 
+eval `ssh-agent`
+ssh-add ~/.ssh/intgeritee_rsa
+
 yarn sync -c config/integritee-kusama.json 2>&1 | tee ./log/yarn_sync.log
